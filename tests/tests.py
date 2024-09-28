27b2e8595ef5666d2b4e39bf17317087c21220fc
KR9SIS <kristoferhelgi@protonmail.com>
2024-09-28 14:51:54 +0000

    Rewrote tests to instead test exe file given to users

--- tests/tests.py
+++ tests/tests.py
@@ -5,19 +5,7 @@ Unittests for the byggja_vakta_toflu.exe application
 import unittest as ut
 from os import chdir
 from pathlib import Path
-from sys import path
-
-path.append(str(Path(__file__).parent.parent.resolve()))
-from exceptions import (
-    DirContentsError,
-    ProgExitError,
-    ShiftsOutOfBoundsError,
-    TakenEmpNameError,
-    UnorthodoxShiftDeniedError,
-    WeekdayNotFoundError,
-    WriteDateError,
-)
-from src import byggja_vakta_toflu as app
+from subprocess import CalledProcessError, run
 
 
 class TestByggjaVaktaTofluExe(ut.TestCase):
@@ -25,60 +13,130 @@ class TestByggjaVaktaTofluExe(ut.TestCase):
     unittest test class
     """
 
+    def extract_error_code(self, err_msg: str) -> int:
+        """
+        Extract ErrorCode from error message and returns it as an integer
+        """
+
+        err_code_ind = err_msg.find("ErrorCode: ")
+        return int(
+            err_msg[
+                err_code_ind
+                + len("ErrorCode: ") : err_code_ind
+                + len("ErrorCode: ")
+                + 2
+            ]
+        )
+
+    def setUp(self):
+        self.program = str(Path("byggja_vakta_toflu.exe").resolve())
+        self.base_command = [self.program, "-s", "-test"]
+        self.errors = {
+            "DirContentsError": -2,
+            "TakenEmpNameError": -3,
+            "WeekdayNotFoundError": -4,
+            "ShiftsOutOfBoundsError": -5,
+            "UnorthodoxShiftDeniedError": -6,
+            "WriteDateError": -7,
+        }
+
     def test_happy_path(self):
         """
         Test that everything works when correct parameters are set
         """
-        app.CreateShiftsSheet(stdout=False)
-        shift_sheet = Path("VaktaTafla.xlsx")
-        self.assertTrue(shift_sheet.exists(follow_symlinks=False))
-        shift_sheet.unlink()
+        self.assertEqual(len([*Path.cwd().iterdir()]), 4)
+        try:
+            run([self.program, "-s"], check=True)
+            shift_sheet = Path("VaktaTafla.xlsx")
+            self.assertTrue(shift_sheet.exists(follow_symlinks=False))
+            shift_sheet.unlink()
+            self.assertEqual(len([*Path.cwd().iterdir()]), 4)
+        except CalledProcessError:
+            pass
 
     def test_wrong_directory(self):
         """
         Test that the program fails when wrong amount of files are in the directory
         """
+        self.assertEqual(len([*Path.cwd().iterdir()]), 4)
         extra_file = Path("EXTRA.txt")
         extra_file.touch()
-        with self.assertRaises(DirContentsError):
-            app.CreateShiftsSheet(test_run=True, stdout=False)
+
+        self.assertEqual(len([*Path.cwd().iterdir()]), 5)
+        try:
+            _ = run(self.base_command, check=True, capture_output=True)
+        except CalledProcessError as exc:
+            err_code = self.extract_error_code(exc.stderr.decode())
+            self.assertEqual(err_code, self.errors["DirContentsError"])
 
         extra_file.unlink()
+        self.assertEqual(len([*Path.cwd().iterdir()]), 4)
 
     def test_emp_name_in_use(self):
         """
         Test that program fails when too many employees are named the same name
         """
-        with self.assertRaises(TakenEmpNameError):
-            app.CreateShiftsSheet(
-                test_run=True, stdout=False, vinna_excel="../ve_duplicate_emp.xlsx"
+        try:
+            cmd = self.base_command
+            cmd.extend(["-ve", "../ve_duplicate_emp.xlsx"])
+            _ = run(
+                cmd,
+                check=True,
+                capture_output=True,
             )
+        except CalledProcessError as exc:
+            err_code = self.extract_error_code(exc.stderr.decode())
+            self.assertEqual(err_code, self.errors["TakenEmpNameError"])
 
     def test_shifts_out_of_bounds(self):
         """
         Test to see if the program responds correctly to an empty template.xlsx file
         """
-        with self.assertRaises(ShiftsOutOfBoundsError):
-            app.CreateShiftsSheet(
-                template="../t_empty.xlsx", test_run=True, stdout=False
+        try:
+            cmd = self.base_command
+            cmd.extend(["-t", "../t_empty.xlsx"])
+            _ = run(
+                cmd,
+                check=True,
+                capture_output=True,
             )
+        except CalledProcessError as exc:
+            err_code = self.extract_error_code(exc.stderr.decode())
+            self.assertEqual(err_code, self.errors["ShiftsOutOfBoundsError"])
 
     def test_unorthodox_shifts_denied(self):
-        with self.assertRaises(UnorthodoxShiftDeniedError):
-            app.CreateShiftsSheet(
-                test_run=True, stdout=False, template="../t_no_adrir_timar.xlsx"
+        """
+        Test if program raises right exception when Aðrir Tímar is not located in template.xlsx
+        """
+        try:
+            cmd = self.base_command
+            cmd.extend(["-t", "../t_no_adrir_timar.xlsx"])
+            _ = run(
+                cmd,
+                check=True,
+                capture_output=True,
             )
+        except CalledProcessError as exc:
+            err_code = self.extract_error_code(exc.stderr.decode())
+            self.assertEqual(err_code, self.errors["UnorthodoxShiftDeniedError"])
 
-    def test_weekday_not_found(self):
+    def test_write_date_error(self):
         """
         Test whether weekdays match between template and vinna excel sheet
         """
-        with self.assertRaises(WriteDateError):
-            app.CreateShiftsSheet(
-                test_run=True, stdout=False, template="../t_wrong_weekday.xlsx"
+        try:
+            cmd = self.base_command
+            cmd.extend(["-t", "../t_wrong_weekday.xlsx"])
+            _ = run(
+                cmd,
+                check=True,
+                capture_output=True,
             )
+        except CalledProcessError as exc:
+            err_code = self.extract_error_code(exc.stderr.decode())
+            self.assertEqual(err_code, self.errors["WriteDateError"])
 
 
 if __name__ == "__main__":
     chdir(Path("test_env").resolve())
-    ut.main(verbosity=2)
+    ut.main(verbosity=1)
